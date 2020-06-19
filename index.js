464545f0c9a19682b925579160bbc79b9e01652b
Gvan-Iovnov <60030514+Gvan-Iovnov@users.noreply.github.com>
2020-06-19 19:42:57 +0300

    Revert "added eslint and prettier + bug fix"
    This reverts commit 9935e76b5ee508f17fd502d5c34c2fc7e4add2d5.

--- index.js
+++ index.js
@@ -1,43 +1,38 @@
-const storage = localStorage;
-const containerForTheList = document.querySelector("#container");
-const listOfTasks = document.createElement("ul");
-containerForTheList.appendChild(listOfTasks);
-const txetFieldForTasks = document.createElement("input");
-txetFieldForTasks.type = "text";
-document.body.appendChild(txetFieldForTasks);
-const addTaskButton = document.createElement("input");
-addTaskButton.type = "button";
-addTaskButton.value = "Далее";
-document.body.appendChild(addTaskButton);
+let storage = localStorage;
+let container = document.querySelector('#container')
+let list = document.createElement('ul');
+container.appendChild(list);
+let txt = document.createElement('input');
+txt.type = 'text';
+document.body.appendChild(txt);
+let btn = document.createElement('input');
+btn.type = 'button';
+btn.value = 'Далее';
+document.body.appendChild(btn);
 // storage.setItem('tasks', '')
-
-function updateList(s) {
-  listOfTasks.innerHTML = "";
-  let arrayOfTasks = s.split(",NEXTTASK");
-  arrayOfTasks = arrayOfTasks.sort();
-  arrayOfTasks.shift();
-  arrayOfTasks.map((task) => {
-    listOfTasks.innerHTML += `<li> ${task} </li>`;
-    return 0;
-  });
+window.onload = function(){
+	if(storage.getItem('tasks'))updateList(storage.getItem('tasks'));
+	else storage.setItem('tasks', '');
 }
-
-function createTask() {
-  const task = txetFieldForTasks.value;
-  if (task.length === 0) return;
-  txetFieldForTasks.value = "";
-  storage.setItem("tasks", `${storage.getItem("tasks")}${task},NEXTTASK`);
-  updateList(storage.getItem("tasks"));
+btn.onclick = function(){
+	createTask();
 }
-
-window.onload = function onWindowLoad() {
-  if (storage.getItem("tasks")) updateList(storage.getItem("tasks"));
-  else storage.setItem("tasks", "");
-};
-addTaskButton.onclick = function buttonClicked() {
-  createTask();
-};
-
-document.addEventListener("keydown", function enterKeyPressed(key) {
-  if (key.which === 13) createTask();
+function updateList(s){
+	list.innerHTML ='';
+	let arrOfTasks = s.split(' ');
+	arrOfTasks = arrOfTasks.sort();
+	arrOfTasks.shift();
+	for(task of arrOfTasks){
+		list.innerHTML += `<li> ${task} </li>`;
+	}
+}
+document.addEventListener('keydown', function(e){
+	 if(e.which==13) createTask();
 });
+function createTask(){
+	let task = txt.value;
+	if(task.length==0) return; 
+	txt.value = '';
+	storage.setItem('tasks', storage.getItem('tasks')+ `${task} `);
+	updateList(storage.getItem('tasks'));
+}
